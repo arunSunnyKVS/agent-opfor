362bbf37ddb740763c5c66fe5395a9a013d94d90
achuvyas-kv <achu@keyvalue.systems>
2026-04-28 14:36:23 +0530

    chore: remove deprecated files and configurations from Siren MCP project

--- astra-mcp/Siren-MCP-dev/src/schema/recipient.schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const recipientSchema = z.object({
-    channel: z.string().describe("The channel to send the notification to [EMAIL, SMS, SLACK, WHATSAPP, DISCORD]"),
-    name: z.string().describe("The name of the user to find"),
-});
-
-
-export type RecipientInput = z.infer<typeof recipientSchema>;
